c847bc6b8c26ebac2a9e50bda4c12f05cb9fb58f
DevDom3105 <devdom3105@gmail.com>
2024-02-17 16:10:58 +0100

    cleaning

--- addons/imjp94.yafsm/src/states/StateWorker.gd
+++ addons/imjp94.yafsm/src/states/StateWorker.gd
@@ -1,8 +1,6 @@
-extends Node
 class_name StateWorker
+extends Node
 
-#export(Resource) var global_transition_Fcond setget _set_fcond_res
-#var _fcond_resource = null
 var _smp = null
 
 func enter():
@@ -14,7 +12,6 @@ func update():
 func exit():
 	pass
 
-
 func external(ref):
 	return self._smp.external(ref)
 
@@ -22,13 +19,3 @@ func get_class():
 	# Used by statemachine player to identify a node as StateWorker, 
 	# so do not override this function in inheriting classes
 	return "StateWorker"
-
-
-
-
-#func _set_fcond_res(val):
-#	global_transition_Fcond = val
-#	if val != null:
-#		_fcond_resource = val.new()
-#	else:
-#		_fcond_resource = null
