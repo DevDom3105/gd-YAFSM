6f96dccce447520cb1549a97e2d5da9d55c15226
DevDom3105 <devdom3105@gmail.com>
2024-02-17 16:31:05 +0100

    docs fcondition

--- addons/imjp94.yafsm/src/conditions/FunctionCondition.gd
+++ addons/imjp94.yafsm/src/conditions/FunctionCondition.gd
@@ -13,9 +13,9 @@ func get_previous_state():
 	return _smp.get_state_worker(_smp.get_previous())
 
 func condition():
-	# User defined FunctionConditions inheriting from this class need to overwrite this function.
-	# It will be called when the transition to which this FunctionCondition belongs will be checked.
-	# The external() function can be called as in the StateWorker class to access any relevant node as 
-	# defined in the statemachineplayer, so any relevant information can be accessed to conclude
-	# whether the condition is met.
+	## User defined FunctionConditions inheriting from this class need to overwrite this function.
+	## It will be called when the transition to which this FunctionCondition belongs will be checked.
+	## The external() function can be called to access any relevant external node as 
+	## defined in the StateMachinePlayer. This way, any relevant information can be accessed to 
+	## define whether the condition is met.
 	pass
